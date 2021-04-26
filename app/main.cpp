02b10ddc02c6ca1906e39495413eb31ef891c452
Ilia Sheshukov <sheshukov.ilya@gmail.com>
2021-04-26 21:50:59 +0300

    new version

--- app/main.cpp
+++ app/main.cpp
@@ -5,12 +5,12 @@
 #include "mymath.hpp"
 
 /*
- * MaxAlgebra -- класс, реализующий арифметику в max-алгебре.
+ * MaxTimes -- класс, реализующий арифметику в max-алгебре.
  * MatrixDn -- матрица, скаляры которой находятся в max-алгебре.
  * ma -- просто сокращение для удобства написания.
  */
-using MatrixDn = Eigen::Matrix<MaxAlgebra, Eigen::Dynamic, Eigen::Dynamic>;
-using ma = MaxAlgebra;
+using MatrixDn = Eigen::Matrix<MaxTimes, Eigen::Dynamic, Eigen::Dynamic>;
+using ma = MaxTimes;
 
 /*
  * Данные строки отвечают за различные способы вывода матриц и векторов.
@@ -51,9 +51,9 @@ MatrixDn matrixWithoutColumn(MatrixDn& matrix, unsigned int colToRemove)
 /*
  * Функция вычисления спектрального радиуса матрицы.
  */
-MaxAlgebra spectral_radius(MatrixDn& m) {
+MaxTimes spectral_radius(MatrixDn& m) {
   auto n(m.rows());
-  MaxAlgebra radius(m.trace());
+  MaxTimes radius(m.trace());
   auto m_i(m);
 
   for (long i = 2; i <= n; ++i) {
@@ -73,7 +73,7 @@ MatrixDn pseudo_inverse(const MatrixDn& A)
  * Функция невязки.
  * Delta(A, b) = ...
  */
-MaxAlgebra residual_function(MatrixDn& A, Eigen::Matrix<MaxAlgebra, -1, 1, 0, -1, 1>& b) {
+MaxTimes residual_function(MatrixDn& A, Eigen::Matrix<MaxTimes, -1, 1, 0, -1, 1>& b) {
     auto Api = pseudo_inverse(A);
     auto bpi = pseudo_inverse(b);
 
@@ -82,13 +82,16 @@ MaxAlgebra residual_function(MatrixDn& A, Eigen::Matrix<MaxAlgebra, -1, 1, 0, -1
 
 MatrixDn linearly_independent_system(MatrixDn& A){
     auto result = A;
+    auto num_cols = A.cols();
+    size_t cur_col = 0;
 
-    for (int i = 0; i < A.cols(); ++i){
-        auto matrix_without_i_col = matrixWithoutColumn(A, i);
-        auto col = A.col(i).eval();
+    for (int i = 0; i < num_cols; ++i){
+        auto matrix_without_i_col = matrixWithoutColumn(result, cur_col);
+        auto col = result.col(cur_col).eval();
         if (residual_function(matrix_without_i_col, col) == 1) {
-            // delete vector
             result = matrix_without_i_col;
+        } else {
+            cur_col++;
         }
     }
 
@@ -116,9 +119,10 @@ MatrixDn cleany(MatrixDn& m) {
  * Функция для получения коэфициентов k и l в задаче нахождения наилучшего
  * дифференцирующего вектора.
  */
-std::pair<MatrixDn::Index, MatrixDn::Index> best_diff_vector_coefficients(
+std::vector<std::pair<MatrixDn::Index, MatrixDn::Index>> best_diff_vector_coefficients(
     MatrixDn& P) {
-  std::vector<MaxAlgebra> k_candidates;
+  std::vector<std::pair<MatrixDn::Index, MatrixDn::Index>> result;
+  std::vector<MaxTimes> k_candidates;
   auto rows = P.rows();
   auto cols = P.cols();
   auto ones_vec = MatrixDn::Ones(rows, 1);
@@ -130,15 +134,21 @@ std::pair<MatrixDn::Index, MatrixDn::Index> best_diff_vector_coefficients(
     k_candidates.push_back(pre_result.value());
   }
 
-  auto k =
-      std::distance(k_candidates.begin(),
-                    std::max_element(k_candidates.begin(), k_candidates.end()));
+  auto max_k_value = *std::max_element(k_candidates.begin(), k_candidates.end());
+  for (MatrixDn::Index k = 0; k < k_candidates.size(); ++k){
+      if (max_k_value == k_candidates[k]) {
+          auto p_k_inv = P.col(k).cwiseInverse().eval();
+          auto max_l_value = p_k_inv.maxCoeff();
+          for (MatrixDn::Index l = 0; l < p_k_inv.size(); ++l){
+              if (max_l_value == p_k_inv(l)){
+                  result.push_back({k, l});
+              }
+          }
+      }
+  }
 
-  auto p_k_inv = P.col(k).cwiseInverse().eval();
-  MatrixDn::Index l_index;
-  p_k_inv.maxCoeff(&l_index);
 
-  return {k, l_index};
+  return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -152,13 +162,13 @@ int main(int argc, char* argv[]) {
    *
    *    Создание матрицы 2x2 с дробными элементами элементами.
    *        MatrixDn A(2,2);
-   *        A << MaxAlgebra(1, 2), 2, MaxAlgebra(1, 3), 4;
+   *        A << MaxTimes(1, 2), 2, MaxTimes(1, 3), 4;
    *
    *    Запись 3^2
-   *        pow(MaxAlgebra(3), 2);
+   *        pow(MaxTimes(3), 2);
    *
    *    1/(3^2)
-   *        1 / pow(MaxAlgebra(3), 2))
+   *        1 / pow(MaxTimes(3), 2))
    *
    *    Матрица из единиц, нулей размерности N
    *        MatrixDn::Ones(N, N);
@@ -168,12 +178,15 @@ int main(int argc, char* argv[]) {
    *        MatrixDn::Identity(N, N);
    */
   MatrixDn C(6, 6);
-  C << 1, MaxAlgebra(1, 4), MaxAlgebra(1, 5), MaxAlgebra(1, 4), 6,
-      MaxAlgebra(1, 6), 4, 1, MaxAlgebra(1, 3), 3, 6, MaxAlgebra(1, 2), 5, 3, 1,
-      4, 7, 3, 4, MaxAlgebra(1, 3), MaxAlgebra(1, 4), 1, 5, MaxAlgebra(1, 5),
-      MaxAlgebra(1, 6), MaxAlgebra(1, 6), MaxAlgebra(1, 7), MaxAlgebra(1, 5), 1,
-      MaxAlgebra(1, 7), 6, 2, MaxAlgebra(1, 3), 5, 7, 1;
+  C << 1, MaxTimes(1, 4), MaxTimes(1, 5), MaxTimes(1, 4), 6,
+      MaxTimes(1, 6), 4, 1, MaxTimes(1, 3), 3, 6, MaxTimes(1, 2), 5, 3, 1,
+      4, 7, 3, 4, MaxTimes(1, 3), MaxTimes(1, 4), 1, 5, MaxTimes(1, 5),
+      MaxTimes(1, 6), MaxTimes(1, 6), MaxTimes(1, 7), MaxTimes(1, 5), 1,
+      MaxTimes(1, 7), 6, 2, MaxTimes(1, 3), 5, 7, 1;
 
+  const auto num_crit_mat = C.rows();
+
+  std::vector<MatrixDn>As;
   MatrixDn A1(3, 3);
   A1 << 1, 5, 8, ma(1, 5), 1, 5, ma(1, 8), ma(1, 5), 1;
   MatrixDn A2(3, 3);
@@ -187,104 +200,200 @@ int main(int argc, char* argv[]) {
   MatrixDn A6(3, 3);
   A6 << 1, 7, 9, ma(1, 7), 1, 7, ma(1, 9), ma(1, 7), 1;
 
+  As.push_back(A1);
+  As.push_back(A2);
+  As.push_back(A3);
+  As.push_back(A4);
+  As.push_back(A5);
+  As.push_back(A6);
+
+  const auto num_comp_mat = A1.rows();
+
   /*
    * Вычисление спектрального радиуса
    */
-  std::cout << "Вычисление спектрального радиуса" << std::endl;
+  std::cout << "Computing spectrail radius: ";
   auto lambda = spectral_radius(C);
-  std::cout << "lambda = " << lambda << std::endl << std::endl;
+  std::cout << "lambda = " << lambda << std::endl;
 
   /*
    * Вычисление (\lambda^{-1}C)^*
    */
-  std::cout << "Вычисление (\\lambda^{-1}C)^*" << std::endl;
-  MatrixDn Calmoststar = 1 / lambda * C;
+  std::cout << "Computing (1/lambda * C)^*" << std::endl;
+  MatrixDn Calmoststar = (1 / lambda) * C;
   auto w = cleany(Calmoststar);
-  std::cout << "w = " << w.format(MatlabFmt) << std::endl << std::endl;
+  std::cout << "(1/lambda * C)^* = \n" << w.format(MatlabFmt) << std::endl << std::endl;
 
   /*
    * Вычисление \delta
    */
-  std::cout << "Вычисление \\delta" << std::endl;
-  auto delta = (MatrixDn::Ones(1, 6) * w * MatrixDn::Ones(6, 1)).value();
+  std::cout << "Computing delta: " << std::endl;
+  auto delta = (MatrixDn::Ones(1, num_crit_mat) * w * MatrixDn::Ones(num_crit_mat, 1)).value();
   // .value() нужно, чтобы достать скаляр из матрицы 1x1.
-  std::cout << "delta = " << delta << std::endl;
-  std::cout << "delta^-1 = " << 1 / delta << std::endl << std::endl;
+  std::cout << "delta = " << delta;
+  std::cout << ", delta^-1 = " << 1 / delta << std::endl << std::endl;
 
   /*
-   * Вычисление наихудшего дифференцирующего вектора
+   * Вычисление наихудшего дифференцирующего вектора весов
    */
-  std::cout << "Вычисление наихудшего дифференцирующего вектора" << std::endl;
-  MatrixDn W1m = (1 / delta * MatrixDn::Ones(6, 6)) + Calmoststar;
+  std::cout << "Computing the worst differentiating weight vector" << std::endl;
+  MatrixDn W1m = (1 / delta * MatrixDn::Ones(num_crit_mat, num_crit_mat)) + Calmoststar;
   auto w1 = cleany(W1m);
-  std::cout << "w1 = " << w1.format(MatlabFmt) << std::endl << std::endl;
+  auto liw1 = linearly_independent_system(w1);
+  std::cout << "Linearly independent w1 = \n" << liw1.format(MatlabFmt) << std::endl << std::endl;
 
-  /* Получилась матрица из двух ЛН векторов. Попробуем получить
-   * "самый наихудший" вектор.
-   * Возьмём первые два столбца (я знаю, что они ЛН), отнормируем по максимуму
+
+  /* Попробуем получить "самый наихудший" вектор.
+   * Если на предыдущем шаге получился единственный вектор, то
+   * данная процедура просто его нормирует.
+   *
+   * Иначе возьмём линейно независимые столбцы матрицы w1, отнормируем по максимуму
    * и сложим.
    */
 
-  auto worst_vector =
-      (w.col(0) / w.col(0).maxCoeff()) + (w.col(1) / w.col(1).maxCoeff());
-  std::cout << "worst_vector = " << worst_vector.format(MatlabFmt) << std::endl
+  MatrixDn worst_vector = MatrixDn::Zero(liw1.rows(), 1);
+  for (int i = 0; i < liw1.cols(); ++i){
+      worst_vector += liw1.col(i) / liw1.col(i).maxCoeff();
+  }
+  std::cout << "Worst differentiating weight vector = \n" << worst_vector.format(MatlabFmt) << std::endl
             << std::endl;
 
   /*
-   * Получим взвешенную сумму.
+   * Получим взвешенную сумму для наихудшего вектора.
    */
 
-  auto B1 =
-      (worst_vector(0) * A1 + worst_vector(1) * A2 + worst_vector(2) * A3 +
-       worst_vector(3) * A4 + worst_vector(4) * A5 + worst_vector(5) * A6)
-          .eval();
+  MatrixDn D1 = MatrixDn::Zero(num_comp_mat, num_comp_mat);
+  for (int i = 0; i < worst_vector.rows(); ++i){
+      D1 += worst_vector(i) * As[i];
+  }
 
-  auto nu = spectral_radius(B1);
-  std::cout << "B1 = " << B1.format(MatlabFmt) << std::endl;
-  std::cout << "nu = " << nu << std::endl;
+  auto nu_1 = spectral_radius(D1);
+  std::cout << "D1 = " << D1.format(MatlabFmt) << std::endl;
+  std::cout << "nu_1 = " << nu_1 << std::endl;
+  MatrixDn almost_x1 = (1 / nu_1) * D1;
 
-  MatrixDn almost_x1 = 1 / nu * B1;
+  /*
+   *  Вектор рейтингов альтернатив для D1
+   */
+
+  std::cout << "\nWorst differentiating vector of alternatives" << std::endl;
+
   auto x1 = cleany(almost_x1);
+  auto LI_x1 = linearly_independent_system(x1);
 
-  std::cout << "x1 = " << x1.format(MatlabFmt) << std::endl;
+  if (LI_x1.cols() == 1){
+      // Решение единственно.
+
+      std::cout << "x1 = \n" << LI_x1.format(MatlabFmt) << std::endl;
+  } else {
+      // Случай неединственности решения. Наихудший дифференцирующий вектор.
+
+      auto delta_1 = (MatrixDn::Ones(1, x1.rows()) * x1 * MatrixDn::Ones(x1.cols(), 1)).value();
+      MatrixDn almost_x1new = ((1/delta_1) * MatrixDn::Ones(x1.cols(), x1.rows())) + almost_x1;
+      auto x1new = cleany(almost_x1new);
+
+      auto LI_x1new = linearly_independent_system(x1new);
+
+      if (LI_x1new.cols() == 1) {
+        std::cout << "x1 = \n" << LI_x1new.format(MatlabFmt) << std::endl;
+      } else {
+      /*
+       *  Если всё равно получается не единственный вектор, то найдём
+       *  "самый плохой" вектор рейтингов альтернатив.
+       */
+
+          MatrixDn worst_x1 = MatrixDn::Zero(LI_x1new.rows(), 1);
+          for (int i = 0; i < LI_x1new.cols(); ++i){
+              worst_x1 += LI_x1new.col(i) / LI_x1new.col(i).maxCoeff();
+          }
+          std::cout << "x1 = \n" << worst_x1.format(MatlabFmt) << std::endl;
+
+      }
+
+  }
+
+  std::cout << std::endl;
 
   /*
-   * Случай неединственности решения.
-   */
-
-  auto delta1 = (MatrixDn::Ones(1, 3) * x1 * MatrixDn::Ones(3, 1)).value();
-  MatrixDn almost_x1new = (delta1 * MatrixDn::Ones(3, 3)) + almost_x1;
-  auto x1new = cleany(almost_x1new);
-  std::cout << "x1new = " << x1new.format(MatlabFmt) << std::endl;
-
-  /*
-   * Вычисление наилучшего дифференцирующего вектора.
+   * Вычисление наилучшего дифференцирующего вектора весов.
    * Матрица P --- линейно-независимые столбцы матрицы w.
-   * Временно, получение линейно-независимых столбцов происходит
-   * при помощи отдельного скрипта Wolfram Mathematica.
    */
-  std::cout << "Вычисление наилучшего дифференцирующего вектора" << std::endl;
-  MatrixDn P(6, 2);
-  P << w.col(0), w.col(1);
-  std::cout << "P = " << P.format(MatlabFmt) << std::endl << std::endl;
+  std::cout << "\nComputing the best differenting weight vector" << std::endl;
+  MatrixDn P = linearly_independent_system(w);
+  std::cout << "P = \n" << P.format(MatlabFmt) << std::endl << std::endl;
 
   /*
    * Получение коэфициентов k и l.
    */
-  std::cout << "Получение коэфициентов k и l" << std::endl;
-  auto [k, l] = best_diff_vector_coefficients(P);
-  std::cout << "k = " << k << ", l = " << l << std::endl << std::endl;
+  auto bdfc = best_diff_vector_coefficients(P);
+  std::cout << "Computing w_2" << std::endl;
+  MatrixDn W2;
+  for (auto& [k, l]: bdfc){
+      /*
+       * Вычисление w_2. Здесь получим матрицу для каждой пары (k, l),
+       * объединим их в одну и найдём ЛН часть.
+       */
+
+      MatrixDn Plk_inverse = MatrixDn::Zero(P.rows(), P.cols());
+      Plk_inverse(l, k) = 1 / P(l, k);
+      auto t = P * (MatrixDn::Identity(P.cols(), P.cols()) +
+                     Plk_inverse.transpose() * P);
+      W2.conservativeResize(t.rows(), W2.cols() + t.cols());
+      W2.rightCols(t.cols()) = t;
+  }
+  auto LI_W2 = linearly_independent_system(W2);
+  std::cout << "\nLinearly independent w2 = \n" << LI_W2.format(MatlabFmt) << std::endl;
 
   /*
-   * Вычисление w_2.
+   *  Тут не совсем ясно что делать, если получается несколько векторов.
+   *  Пока возьму первый
    */
-  std::cout << "Вычисление w_2" << std::endl;
-  MatrixDn Plk_inverse = MatrixDn::Zero(P.rows(), P.cols());
-  Plk_inverse(l, k) = 1 / P(l, k);
 
-  auto w2 = P * (MatrixDn::Identity(P.cols(), P.cols()) +
-                 Plk_inverse.transpose() * P);
+  auto w2 = LI_W2.col(0);
 
-  std::cout << "w2 = " << w2.format(MatlabFmt) << std::endl;
+  MatrixDn D2 = MatrixDn::Zero(num_comp_mat, num_comp_mat);
+  for (int i = 0; i < w2.rows(); ++i){
+      D2 += w2(i) * As[i];
+  }
+
+  /*
+   *  Вычислияем вектор рейтингов альтернатив для D_2.
+   */
+
+  std::cout << "\nComputing the best differentiating vector of alternatives" << std::endl;
+
+  auto nu_2 = spectral_radius(D2);
+  std::cout << "D2 = " << D2.format(MatlabFmt) << std::endl;
+  std::cout << "nu_2 = " << nu_2 << std::endl;
+  MatrixDn almost_x2 = (1 / nu_2) * D2;
+
+  auto x2 = cleany(almost_x2);
+  auto LI_x2 = linearly_independent_system(x2);
+
+  std::cout << "\nBest differentiang vector of alternatives"  << std::endl;
+  if (LI_x2.cols() == 1){
+  // Случай единственности
+      std::cout << "x2 = " << LI_x2.format(MatlabFmt) << std::endl;
+  } else {
+  // Случай неединственности
+      auto S = LI_x2;
+      auto bdfc = best_diff_vector_coefficients(S);
+      MatrixDn WS;
+      for (auto& [k, l]: bdfc){
+          MatrixDn Slk_inverse = MatrixDn::Zero(S.rows(), S.cols());
+          Slk_inverse(l, k) = 1 / S(l, k);
+          auto t = S * (MatrixDn::Identity(S.cols(), S.cols()) +
+                         Slk_inverse.transpose() * S);
+          WS.conservativeResize(t.rows(), WS.cols() + t.cols());
+          WS.rightCols(t.cols()) = t;
+      }
+      auto LI_WS = linearly_independent_system(WS);
+      std::cout << "x2 = " << LI_WS.format(MatlabFmt) << std::endl;
+  }
+
+  /*
+   *  Тут не совсем ясно что делать, если получается несколько векторов.
+   */
+
   return 0;
 }
--- lib/mymath/include/mymath.hpp
+++ lib/mymath/include/mymath.hpp
@@ -4,6 +4,7 @@
 #include <Eigen/Core>
 #include <ostream>
 
+template<typename Op>
 class MaxAlgebra {
  public:
   GiNaC::ex value;
@@ -13,45 +14,88 @@ class MaxAlgebra {
   MaxAlgebra(GiNaC::ex val) : value(val){};
   MaxAlgebra(int num, int den) : value(GiNaC::numeric(num, den)){};
 
-  friend std::ostream& operator<<(std::ostream& out, const MaxAlgebra& val);
-  MaxAlgebra& operator+=(const MaxAlgebra& rhs);
-  MaxAlgebra& operator=(const MaxAlgebra& rhs);
-  MaxAlgebra& operator*=(const MaxAlgebra& rhs);
-  MaxAlgebra& operator/=(const MaxAlgebra& rhs);
-  MaxAlgebra& abs(const MaxAlgebra& rhs);
+  friend MaxAlgebra operator+(const MaxAlgebra& lhs, const MaxAlgebra& rhs){
+      return (lhs < rhs) ? rhs : lhs;
+  };
+
+  friend MaxAlgebra operator*(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
+  friend MaxAlgebra operator/(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
+
+
+  friend std::ostream& operator<<(std::ostream& out, const MaxAlgebra& val) {
+      using std::operator<<;
+      out << val.value;
+      return out;
+  }
+  MaxAlgebra& operator=(const MaxAlgebra& rhs) {
+      this->value = rhs.value;
+      return *this;
+  };
+
+  MaxAlgebra& operator+=(const MaxAlgebra& rhs) {
+      *this = *this + rhs;
+      return *this;
+  };
+  MaxAlgebra& operator*=(const MaxAlgebra& rhs) {
+      *this = this * rhs;
+      return *this;
+  };
+  MaxAlgebra& operator/=(const MaxAlgebra& rhs) {
+      *this = this / rhs;
+      return *this;
+  };
+
+  friend bool operator<(const MaxAlgebra& lhs, const MaxAlgebra& rhs) {
+      return lhs.value.evalf() < rhs.value.evalf();
+  };
+  friend bool operator==(const MaxAlgebra& lhs, const MaxAlgebra& rhs) {
+      return lhs.value == rhs.value;
+  };
+  friend bool operator>(const MaxAlgebra& lhs, const MaxAlgebra& rhs) {
+      return !(lhs < rhs);
+  };
+
+
+  MaxAlgebra& abs(const MaxAlgebra& rhs) { return MaxAlgebra(GiNaC::abs(rhs.value)); }
 
   explicit operator GiNaC::ex() { return value; }
-  friend MaxAlgebra pow(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
+  friend MaxAlgebra pow(const MaxAlgebra& lhs, const MaxAlgebra& rhs) {
+      return GiNaC::pow(lhs.value, rhs.value);
+  };
+  friend bool isfinite(const MaxAlgebra&);
 };
 
-#define OVERLOAD_OPERATOR_DECL(op, ret) \
-  ret operator op(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
+MaxAlgebra<std::plus<void>> operator*(const MaxAlgebra<std::plus<void>>& lhs,
+                                      const MaxAlgebra<std::plus<void>>& rhs) {
+    return MaxAlgebra<std::plus<void>>(lhs.value + rhs.value);
 
-#define OVERLOAD_OPERATOR_BOOL(op)                                 \
-  bool operator op(const MaxAlgebra& lhs, const MaxAlgebra& rhs) { \
-    return lhs.value op rhs.value;                                 \
-  }
+}
+MaxAlgebra<std::plus<void>> operator/(const MaxAlgebra<std::plus<void>>& lhs,
+                                      const MaxAlgebra<std::plus<void>>& rhs) {
+    return MaxAlgebra<std::plus<void>>(lhs.value - rhs.value);
+}
 
-OVERLOAD_OPERATOR_DECL(/, MaxAlgebra);
-OVERLOAD_OPERATOR_DECL(*, MaxAlgebra);
+MaxAlgebra<std::multiplies<void>> operator*(const MaxAlgebra<std::multiplies<void>>& lhs,
+                                            const MaxAlgebra<std::multiplies<void>>& rhs) {
+    return MaxAlgebra<std::multiplies<void>>(lhs.value * rhs.value);
+}
+MaxAlgebra<std::multiplies<void>> operator/(const MaxAlgebra<std::multiplies<void>>& lhs,
+                                            const MaxAlgebra<std::multiplies<void>>& rhs) {
+    return MaxAlgebra<std::multiplies<void>>(lhs.value / rhs.value);
+}
 
-MaxAlgebra operator+(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
-bool operator<(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
-bool operator==(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
-bool operator>(const MaxAlgebra& lhs, const MaxAlgebra& rhs);
+using MaxTimes = MaxAlgebra<std::multiplies<void>>;
+using MaxPlus = MaxAlgebra<std::plus<void>>;
 
-bool isfinite(const MaxAlgebra&);
 
 namespace Eigen {
 template <>
-struct NumTraits<MaxAlgebra>
-    : GenericNumTraits<MaxAlgebra>  // permits to get the epsilon,
-                                    // dummy_precision, lowest, highest
-                                    // functions
+struct NumTraits<MaxTimes>
+    : GenericNumTraits<MaxTimes>
 {
-  typedef MaxAlgebra Real;
-  typedef MaxAlgebra NonInteger;
-  typedef MaxAlgebra Nested;
+  typedef MaxTimes Real;
+  typedef MaxTimes NonInteger;
+  typedef MaxTimes Nested;
   static inline int digits10() { return 0; }
 
   enum {
@@ -66,30 +110,70 @@ struct NumTraits<MaxAlgebra>
 };
 
 template <typename BinaryOp>
-struct ScalarBinaryOpTraits<MaxAlgebra, GiNaC::ex, BinaryOp> {
-  typedef MaxAlgebra ReturnType;
+struct ScalarBinaryOpTraits<MaxTimes, GiNaC::ex, BinaryOp> {
+  typedef MaxTimes ReturnType;
 };
 
 template <typename BinaryOp>
-struct ScalarBinaryOpTraits<GiNaC::ex, MaxAlgebra, BinaryOp> {
-  typedef MaxAlgebra ReturnType;
+struct ScalarBinaryOpTraits<GiNaC::ex, MaxTimes, BinaryOp> {
+  typedef MaxTimes ReturnType;
 };
 template <typename BinaryOp>
-struct ScalarBinaryOpTraits<MaxAlgebra, GiNaC::numeric, BinaryOp> {
-  typedef MaxAlgebra ReturnType;
+struct ScalarBinaryOpTraits<MaxTimes, GiNaC::numeric, BinaryOp> {
+  typedef MaxTimes ReturnType;
 };
 
 template <typename BinaryOp>
-struct ScalarBinaryOpTraits<GiNaC::numeric, MaxAlgebra, BinaryOp> {
-  typedef MaxAlgebra ReturnType;
+struct ScalarBinaryOpTraits<GiNaC::numeric, MaxTimes, BinaryOp> {
+  typedef MaxTimes ReturnType;
 };
+
+template <>
+struct NumTraits<MaxPlus>
+    : GenericNumTraits<MaxPlus>
+{
+  typedef MaxPlus Real;
+  typedef MaxPlus NonInteger;
+  typedef MaxPlus Nested;
+  static inline int digits10() { return 0; }
+
+  enum {
+    IsComplex = 0,
+    IsInteger = 0,
+    IsSigned = 1,
+    RequireInitialization = 1,
+    ReadCost = 1,
+    AddCost = 3,
+    MulCost = 3
+  };
+};
+
+template <typename BinaryOp>
+struct ScalarBinaryOpTraits<MaxPlus, GiNaC::ex, BinaryOp> {
+  typedef MaxTimes ReturnType;
+};
+
+template <typename BinaryOp>
+struct ScalarBinaryOpTraits<GiNaC::ex, MaxPlus, BinaryOp> {
+  typedef MaxTimes ReturnType;
+};
+template <typename BinaryOp>
+struct ScalarBinaryOpTraits<MaxPlus, GiNaC::numeric, BinaryOp> {
+  typedef MaxTimes ReturnType;
+};
+
+template <typename BinaryOp>
+struct ScalarBinaryOpTraits<GiNaC::numeric, MaxPlus, BinaryOp> {
+  typedef MaxTimes ReturnType;
+};
+
 }  // namespace Eigen
 
 /*
 namespace GiNaC {
 template<>
-MaxAlgebra
-pow<MaxAlgebra, MaxAlgebra>(const MaxAlgebra& a, const MaxAlgebra& b)
+MaxPlus
+pow<MaxPlus, MaxPlus>(const MaxPlus& a, const MaxPlus& b)
 {}
 
 }
